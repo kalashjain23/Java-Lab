d22fa83d249b67cdb0af0976452ca79c35a19d23
kalashjain23 <kalashjain513@gmail.com>
2022-08-05 06:53:29 +0530

    Added half of the diamond pattern code

--- LabWork/src/DiamondPattern.java
+++ LabWork/src/DiamondPattern.java
@@ -3,6 +3,20 @@ import java.util.Scanner;
 public class DiamondPattern {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.print("Enter the length of the diamond: ");
+        int n = in.nextInt(), spaces = n-1;
+
+        for(int i = 0; i < n; i++){
+            for(int j = spaces; j > 0; j--){
+                System.out.print(" ");
+            }
+            for(int j = 2*(n-spaces)-1; j > 0; j--){
+                System.out.print("*");
+            }
+            spaces--;
+            System.out.println();
+        }
+        spaces = 0;
 
     }
 }
