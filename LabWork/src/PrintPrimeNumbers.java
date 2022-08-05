7b0b36c2ad346368070442224606925c74604059
kalashjain23 <kalashjain513@gmail.com>
2022-08-05 06:37:40 +0530

    a minor mistake

--- LabWork/src/PrintPrimeNumbers.java
+++ LabWork/src/PrintPrimeNumbers.java
@@ -6,7 +6,7 @@ public class PrintPrimeNumbers {
         System.out.print("Enter the number of prime numbers you want: ");
         int n = in.nextInt(),count = 1, i = 3;
         System.out.print("2");
-        while(count <cd  n){
+        while(count < n){
             boolean prime = true;
             for(int j = 2; j <= i/2; j++){
                 if(i%j == 0){
